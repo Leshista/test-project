1435e4443097f678ce3d1c20122414b2c3fb08aa
leshista <a.leshista@gmail.com>
2022-03-18 21:19:32 +0300

    Added comments and readme

--- js/script.js
+++ js/script.js
@@ -4,14 +4,18 @@ const modalWindow = document.querySelector('.modal__window');
 const modalId = document.querySelector('.modal__id');
 const modalTurnOff = document.querySelector('.modal__turn-off');
 
-pictures.forEach((e) =>
-    e.addEventListener('click', () => {
-        backdrop.classList.toggle('disabled');
-        modalId.innerHTML = e.id;
-        modalWindow.style.backgroundImage = getComputedStyle(e).backgroundImage;
-    })
+pictures.forEach(
+    (
+        e // Adding an event listener on every clickable picture
+    ) =>
+        e.addEventListener('click', () => {
+            backdrop.classList.toggle('disabled'); // Clicking will enable backdrop
+            modalId.innerHTML = e.id; // Changing the number displayed accordingly to id of the clicked picture
+            modalWindow.style.backgroundImage =
+                getComputedStyle(e).backgroundImage; // Changing the background image of modal window accordingly to the background image of the clicked picture
+        })
 );
 
 modalTurnOff.addEventListener('click', () => {
-    backdrop.classList.toggle('disabled');
+    backdrop.classList.toggle('disabled'); // Disabling backdrop with the click of the button
 });
